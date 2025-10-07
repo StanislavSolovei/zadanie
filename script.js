24503cbdd3f94ad382c1e62aef16464f8812e596
StanislavSolovei <edukacjateb176@gmail.com>
2025-10-07 09:49:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const Name = "Stanislav"
+const Lastname = "Solovei"
+let myNumber = 14
+let randomNumber = 114
+
+alert(Name);
+
+function myLastName() {
+    console.log(Lastname);
+}
+
+function random() {
+    alert()
+    alert()
+}
+
+console.log(myNumber);
+console.log(randomNumber);
